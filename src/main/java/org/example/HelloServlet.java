55517859a4cf22beed6dcd9bd65383c66329fda2
Alexis Salgado <chimalexoficial@gmail.com>
2024-09-05 16:22:19 -0600

    servlet done req,res

--- src/main/java/org/example/HelloServlet.java
+++ src/main/java/org/example/HelloServlet.java
@@ -4,9 +4,16 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class HelloServlet extends HttpServlet {
-    public void service(HttpServletRequest req, HttpServletResponse res) {
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
         System.out.println("In service");
+
+        res.setContentType("text/html");
+
+        PrintWriter out = res.getWriter();
+        out.println("<h2>Hello World!</h2>");
     }
 }
